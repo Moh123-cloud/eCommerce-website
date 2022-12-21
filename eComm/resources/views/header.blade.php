91e9d5d3260f4a03b34b81e0f8ecf9c4dce1b98d
moh <thaiyamoses01@gmail.com>
2022-12-21 23:37:36 +0300

    Migrated and seeded

---
+++ eComm/resources/views/header.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav navbar-nav mr-auto ">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Order</a>
+            </li>
+        </ul>
+        <form class="navbar-form navbar-left">
+            <div class="form-group">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-default" type="submit">Submit</button>
+            </div>
+        </form>
+        <ul class="nav navbar-nav mr-auto navbar-right">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Cart(0)</a>
+            </li>
+        </ul>
+    </div>
+</nav>
