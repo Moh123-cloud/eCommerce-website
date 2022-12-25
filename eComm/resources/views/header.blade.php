bc3a295607548200faa3a6cf9df0c38c3a80cb33
moh <thaiyamoses01@gmail.com>
2022-12-25 12:11:02 +0300

    login and logout done

--- eComm/resources/views/header.blade.php
+++ eComm/resources/views/header.blade.php
@@ -10,7 +10,7 @@ if (Session::has('user')) {
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">E-Comm</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
